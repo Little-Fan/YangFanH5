6f467132a50e1ca8d04b5b37e684f0c44dfd1865
Fan Xiaolong <xiaolong.fan@otvcloud.com>
2016-03-22 19:33:33 +0800

    专辑详情页面
    Change-Id: Ib64befafcdcd070439a70228c0ad066fbe99f3e0

--- public/js/special.js
+++ public/js/special.js
@@ -1,3 +1,12 @@
 /**
  * Created by fanxiaolong on 2016/3/22.
  */
+$(document).ready(function (e) {
+
+    var d1 = $.ajax({
+        method: "GET",
+        url:      '../templates/special/layout.hbs'
+    }).done(function (data) {
+        $('body').html(data);
+    });
+});
