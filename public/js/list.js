cc27b6bd6514d7cfd6cad22a2c0c627e3c77d59e
Fan Xiaolong <xiaolong.fan@otvcloud.com>
2016-03-22 09:28:12 +0800

    空格的处理
    Change-Id: I228f7fe3dc4410b250a4cd28d1ed3114faa1d160

--- public/js/list.js
+++ public/js/list.js
@@ -5,7 +5,6 @@ $(document).ready(function (e) {
 
     var baseURL = 'http://ceshi2.chinacloudapp.cn:8080/rest/rest/'; //接口基准位置
     var id = location.search.substr(1);
-
     var d1 = $.ajax({
         method: "GET",
         url:      '../templates/list/layout.hbs'
