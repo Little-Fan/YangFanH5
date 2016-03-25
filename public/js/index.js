84345cd8f91008bdd0517263481cc596f293317e
Fan Xiaolong <xiaolong.fan@otvcloud.com>
2016-03-25 17:32:27 +0800

    图标位置重设
    Change-Id: I5e9b81cb392aa25754ee562cdb0f0aa7399f1576

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,11 @@
  */
 $(document).ready(function (e) {
     var opts = {
-        lines: 13 // The number of lines to draw
-        , length: 28 // The length of each line
-        , width: 14 // The line thickness
-        , radius: 42 // The radius of the inner circle
-        , scale: 1 // Scales overall size of the spinner
+        lines: 17 // The number of lines to draw
+        , length: 0 // The length of each line
+        , width: 3 // The line thickness
+        , radius: 20 // The radius of the inner circle
+        , scale: 2 // Scales overall size of the spinner
         , corners: 1 // Corner roundness (0..1)
         , color: '#000' // #rgb or #rrggbb or array of colors
         , opacity: 0.25 // Opacity of the lines
