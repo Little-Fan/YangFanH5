313091150a36707f43bde383fad2d1a4923e11ec
Fan Xiao Long <xiaolong.fan@otvcloud.com>
2016-04-07 16:57:43 +0800

    '数组的逗号问题'
    Change-Id: I6472dd087d9124b39459454a580a67743a7de5bb

--- public/js/index.js
+++ public/js/index.js
@@ -3,26 +3,26 @@
  */
 $(document).ready(function () {
     var opts = {
-        lines: 17 // The number of lines to draw
-        , length: 0 // The length of each line
-        , width: 3 // The line thickness
-        , radius: 20 // The radius of the inner circle
-        , scale: 2 // Scales overall size of the spinner
-        , corners: 1 // Corner roundness (0..1)
-        , color: '#000' // #rgb or #rrggbb or array of colors
-        , opacity: 0.25 // Opacity of the lines
-        , rotate: 0 // The rotation offset
-        , direction: 1 // 1: clockwise, -1: counterclockwise
-        , speed: 1 // Rounds per second
-        , trail: 60 // Afterglow percentage
-        , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-        , zIndex: 2e9 // The z-index (defaults to 2000000000)
-        , className: 'spinner' // The CSS class to assign to the spinner
-        , top: '50%' // Top position relative to parent
-        , left: '50%' // Left position relative to parent
-        , shadow: false // Whether to render a shadow
-        , hwaccel: false // Whether to use hardware acceleration
-        , position: 'absolute' // Element positioning
+        lines: 17, // The number of lines to draw
+        length: 0, // The length of each line
+        width: 3, // The line thickness
+        radius: 20, // The radius of the inner circle
+        scale: 2, // Scales overall size of the spinner
+        corners: 1, // Corner roundness (0..1)
+        color: '#000', // #rgb or #rrggbb or array of colors
+        opacity: 0.25, // Opacity of the lines
+        rotate: 0, // The rotation offset
+        direction: 1, // 1: clockwise, -1: counterclockwise
+        speed: 1, // Rounds per second
+        trail: 60, // Afterglow percentage
+        fps: 20, // Frames per second when using setTimeout() as a fallback for CSS
+        zIndex: 2e9, // The z-index (defaults to 2000000000)
+        className: 'spinner', // The CSS class to assign to the spinner
+        top: '50%', // Top position relative to parent
+        left: '50%', // Left position relative to parent
+        shadow: false, // Whether to render a shadow
+        hwaccel: false, // Whether to use hardware acceleration
+        position: 'absolute' // Element positioning
     };
     var target = $('#body-index')[0];  //获取DOM对象
     new Spinner(opts).spin(target);  //loading加载动画
@@ -166,7 +166,6 @@ $(document).ready(function () {
                             var html = template(data2[0]);
                             $(element).next('.list').html(html);
                         });
-
                     });
                 });
             }
