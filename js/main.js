f98078da57bc09af52510ba030fe12106f2cc8ab
Fan Xiao Long <xiaolong.fan@otvcloud.com>
2016-05-10 18:50:07 +0800

    增加hbs模块配置
    Change-Id: Icbe5151ae7787893fb9fc0b417dba64a0a7dd067

--- js/main.js
+++ js/main.js
@@ -4,17 +4,23 @@ require.config({
     baseUrl: "../js",
     paths: {
         jquery: '../node_modules/jquery/dist/jquery',
-        handlebars: '../node_modules/handlebars/dist/handlebars.min',
+        hbs: '../bower_components/require-handlebars-plugin/hbs',
         cookie: '../node_modules/js-cookie/src/js.cookie.js',
         moment: '../node_modules/moment/min/moment.min',
         spin: '../node_modules/spin.js/spin.min',
-        detect: '../node_modules/Detect.js/detect.min'
+        detect: '../node_modules/Detect.js/detect.min',
+        templates: '../templates'
+    },
+    hbs: {
+        helpers: true,            // default: true
+        templateExtension: 'hbs', // default: 'hbs'
+        partialsUrl: ''           // default: ''
     }
 });
 
-
 require([
+    'jquery',
     'app/index'
-], function (index) {
+], function ($, index) {
     console.log(index);
 });
