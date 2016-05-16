04ce4a0b741b48304a840859fd9d509f501c0180
Fan Xiao Long <xiaolong.fan@otvcloud.com>
2016-05-16 12:03:44 +0800

    index页面amd转换完成
    Change-Id: Id35431ffc8bb9a8ed6eebaa8fc0084646336f592

--- www/js/app/index.js
+++ www/js/app/index.js
@@ -8,22 +8,51 @@ define([
     'hbs!templates/index/live-item',
     'hbs!templates/index/list-layout',
     'hbs!templates/index/list-item'
-], function (common, layoutView, commonLoadView, liveLayoutView, categoryItemView, liveItemView, listLayoutView, listItemView) {
+], function (common,
+             layoutView,
+             commonLoadView,
+             liveLayoutView,
+             categoryItemView,
+             liveItemView,
+             listLayoutView,
+             listItemView) {
     common.loading('#body-index');
 
     var type = common.getQueryVariable('type');
     var id = common.getQueryVariable('id');
 
-    function getCategorised(element) {
+    function renderTitle(parentElement, templateView) {
         return $.ajax({
-            method:   'GET',
-            url:      common.baseURL + 'contents/categorys',
+            method: 'GET',
+            url: common.baseURL + 'contents/categorys',
             dataType: 'json',
-            data:     {
+            data: {
                 ParentID: id
             }
         }).done(function (data) {
-            data.$element = element.html(liveLayoutView(data));
+            data.type = type;
+            data.$element = parentElement.html(templateView(data));
+        });
+    }
+
+    function renderMain(data, templateView) {
+        /** @namespace data.Categorys */
+        /** @namespace data.Categorys.Category */
+        $.each(data.Categorys.Category, function (index, obj) {
+            $.ajax({
+                method: 'GET',
+                url: common.baseURL + 'contents/contentlist',
+                dataType: 'json',
+                data: {
+                    CategoryID: obj.CategoryID
+                }
+            }).done(function (contentList) {
+                if (type === 'live') {
+                    data.$element.find('.main').append(templateView(contentList));
+                } else {
+                    data.$element.find('.list').eq(index).html(templateView(contentList));
+                }
+            })
         });
     }
 
@@ -36,11 +65,14 @@ define([
         data.$element = $('#body-index').html(layoutView(data)).find('.content').html(commonLoadView);
     }).done(function (data) {
         if (type === 'live') {
-            getCategorised(data.$element).done(function (data) {
+            renderTitle(data.$element, liveLayoutView).done(function (data) {
                 data.$element.find('.sub-nav').html(categoryItemView(data));
-                console.timeStamp('fan')
             }).done(function (data) {
-                console.timeStamp('fan2')
+                renderMain(data, liveItemView);
+            })
+        } else {
+            renderTitle(data.$element, listLayoutView).done(function (data) {
+                renderMain(data, listItemView)
             })
         }
     })
